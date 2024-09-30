79d350e8d7ac4fdff6491412f25fc831d5342425
Emin Gundogan <emin.gundogan@etu.univ-grenoble-alpes.fr>
2024-09-30 17:30:38 +0200

    test: :white_check_mark: Start task2 tests implem

--- task2/implem/src/tests/Main.java
+++ task2/implem/src/tests/Main.java
@@ -1,15 +1,12 @@
 package tests;
 
 import Interface.Broker;
+import Interface.QueueBroker;
 import implm.Task;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		Broker client = new implm.Broker("client");
-		Broker server = new implm.Broker("server");
-		
-		new Task(client, new Task1TestClient("server", 80)).start();
-		new Task(server, new Task1TestServer(80)).start();
+		//TODO : Start task2 tests
 	}
 }
---
+++ task2/implem/src/tests/Task2TestServer.java
@@ -0,0 +1,45 @@
+package tests;
+
+import Interface.Broker;
+import Interface.Channel;
+import Interface.MessageQueue;
+import Interface.QueueBroker;
+import implm.Task;
+import implm.Channel.DisconnectedException;
+
+public class Task2TestServer implements Runnable{
+	
+	private int port;
+	
+	public Task2TestServer(int port) {
+		this.port = port;
+	}
+	
+	
+	private void work(MessageQueue messageQueue) {
+		try {
+			while(true) {
+				byte[] response = messageQueue.receive();
+				for(int i = 0; i < response.length; i++) {
+					System.out.println("Server receives " + response[i]);
+				}
+				
+				messageQueue.send(response, 0, response.length);
+			}
+		}catch(DisconnectedException e) {
+			System.out.println("Remote channel disconnected");
+		}
+	}
+
+	@Override
+	public void run() {
+		
+		QueueBroker qb = Task.getTask().getQueueBroker();
+		while(true) {
+			MessageQueue messageQueue = qb.accept(port);
+			work(messageQueue);
+		}
+		
+	}
+
+}
