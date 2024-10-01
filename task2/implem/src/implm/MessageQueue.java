1213019a2d041ad84bb93135e8f1ab4059abf319
Emin Gundogan <emin.gundogan@etu.univ-grenoble-alpes.fr>
2024-10-01 08:16:26 +0200

    fix: :bug: Fix array length bug
    Change array length message to avoid sending byte 0

--- task2/implem/src/implm/MessageQueue.java
+++ task2/implem/src/implm/MessageQueue.java
@@ -9,21 +9,56 @@ public class MessageQueue extends Interface.MessageQueue{
 	public MessageQueue(Channel c) {
 		this.c = c;
 	}
+	
+	private byte[] encodeLength(int length) {
+		if(length < 256) {
+			byte[] result = {(byte)length};
+			return result;
+		}
+		
+		byte[] result;
+		if(length < 65536) {
+			result = new byte[2];
+			result[0] = (byte) (length >> 8);
+			result[1] = (byte) length;
+			return result;
+		}
+		if(length < 16777216) {
+			result = new byte[3];
+			result[0] = (byte) (length >> 16);
+			result[1] = (byte) (length >> 8);
+			result[2] = (byte) length;
+		}
+		
+		result = new byte[4];
+		result[0] = (byte) (length >> 24);
+		result[1] = (byte) (length >> 16);
+		result[2] = (byte) (length >> 8);
+		result[3] = (byte) length;
+		return result;
+	}
+	
+	private int decodeLength(byte[] size, int len) {
+		int result = 0;
+	    for (int i = 0; i < len; i++) {
+	        result |= (size[i] & 0xFF) << ((len - i - 1) * 8);
+	    }
+	    return result;
+	}
 
 	@Override
 	public void send(byte[] bytes, int offset, int length) {
-		byte[] size = new byte[4];
-		size[0] = (byte) (length >> 24);
-		size[1] = (byte) (length >> 16);
-		size[2] = (byte) (length >> 8);
-		size[3] = (byte) length;
 		
 		try {
-			int channelStatus = 0;
-			while(channelStatus != 4) {
-				channelStatus += c.write(size, channelStatus, 4 - channelStatus);
-			}
+			byte[] size = encodeLength(length);
+			int len = size.length;
+			byte[] sizeLen = {(byte)len};
+			c.write(sizeLen, 0, 1);
 			
+			int channelStatus = 0;
+			while(channelStatus != len) {
+				channelStatus += c.write(size, channelStatus, len - channelStatus);
+			}
 			channelStatus = 0;
 			while(channelStatus != length) {
 				channelStatus += c.write(bytes, offset + channelStatus, length - channelStatus);
@@ -36,17 +71,17 @@ public class MessageQueue extends Interface.MessageQueue{
 
 	@Override
 	public byte[] receive() {
-		byte[] size = new byte[4];
 		try {
-			int channelStatus = 0;
-			while(channelStatus != 4) {
-				channelStatus += c.read(size, channelStatus, 4 - channelStatus);
-			}
+			byte[] size = new byte[1];
+			c.read(size, 0, 1);
+			int sizeLen = size[0];
 			
-			int len = ((size[0] & 0xFF) << 24) |
-		             ((size[1] & 0xFF) << 16) |
-		             ((size[2] & 0xFF) << 8) |
-		             (size[3] & 0xFF);
+			size = new byte[sizeLen];
+			int channelStatus = 0;
+			while(channelStatus != sizeLen) {
+				channelStatus += c.read(size, channelStatus, sizeLen - channelStatus);
+			}
+			int len = decodeLength(size, sizeLen);
 			
 			channelStatus = 0;
 			byte[] content = new byte[len];
