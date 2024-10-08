5b29e5ad6c93905defc4b6baf51e6c66de9f630e
EminGDGN <Gundogan.emin@laposte.net>
2024-10-08 18:34:28 +0200

    fix: :bug: Remove Task2 tests

--- task3/implem/src/tests/Main.java
+++ task3/implem/src/tests/Main.java
@@ -7,14 +7,6 @@ import implm.Task;
 public class Main {
 	
 	public static void main(String[] args) {
-		Broker client = new implm.Broker("client");
-		Broker server = new implm.Broker("server");
 		
-		QueueBroker bc = new implm.QueueBroker(client);
-		QueueBroker bs = new implm.QueueBroker(server);
-		
-		new Task(bc, new Task2TestClient("server", 80)).start();
-		new Task(bc, new Task2TestClient("server", 80)).start();
-		new Task(bs, new Task2TestServer(80)).start();
 	}
 }
--- task3/implem/src/tests/Task2TestClient.java
+++ task3/implem/src/tests/Task2TestClient.java
@@ -6,32 +6,11 @@ import Interface.QueueBroker;
 import implm.Task;
 
 class Task2TestClient implements Runnable{
-	
-	private String remoteName;
-	private int remotePort;
-	
-	public Task2TestClient(String remoteName, int remotePort) {
-		this.remoteName = remoteName;
-		this.remotePort = remotePort;
-	}
 
 	@Override
 	public void run() {
+		// TODO Auto-generated method stub
 		
-		QueueBroker qb = Task.getTask().getQueueBroker();
-		MessageQueue messageQueue = qb.connect(remoteName, remotePort);
-		byte[] buffer = new byte[255];
-		for(int i = 1; i <= 255; i++) {
-			buffer[i-1] = (byte)i;
-		}
-		
-		messageQueue.send(buffer, 0, 255);
-		byte[] response = messageQueue.receive();
-		for(int i = 0; i < response.length; i++) {
-			System.out.println("Client receives " + Integer.toString(response[i]));
-		}
-		
-		messageQueue.close();
 	}
 
 }
